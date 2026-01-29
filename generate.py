cdf1a4c3ae8759c486898a27867a8b2bb5694773
mizuryo108 <153165016+mizuryo108@users.noreply.github.com>
2026-01-29 14:20:34 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,8 +3,15 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tools", default="tools.json")  # ツールリストの引数を追加
-    parser.add_argument("--input", default="jmultiwoz_tc_input.json")
-    # 入力データの引数を追加
+    parser.add_argument(
+        "--tools",
+        default="tools.json",
+        help="JMultiWOZ-TCに含まれるツールリストのファイルパスを指定",
+    )
+    parser.add_argument(
+        "--input",
+        default="jmultiwoz_tc_input.json",
+        help="JMultiWOZ-TCに含まれる入力データのファイルパスを指定",
+    )
 
     args = parser.parse_args()  # 引数を解析
